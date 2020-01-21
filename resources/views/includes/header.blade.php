6bc08e2c3703a56f534857e9c281ded84bfda483
Rohit599 <rjakhmola5@gmail.com>
2020-01-21 10:16:09 +0530

    Add header restrictions

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -13,7 +13,7 @@
         <li>
           <ul class="collapsible collapsible-accordion">
             <li><a href="{{ url('home') }}" class="waves-effect"><i class="fas fa-home"></i> Home</a></li>
-            <li><a href="{{ url('users') }}" class="waves-effect"><i class="fas fa-users"></i> User</a></li>
+            @if(Auth::user()->is_admin == 1)<li><a href="{{ url('users') }}" class="waves-effect"><i class="fas fa-users"></i> User</a></li>@endif
             <li><a href="{{ url('tncs') }}" class="waves-effect"><i class="fas fa-users"></i> T&Cs</a></li>
 {{--             <li><a class="collapsible-header waves-effect arrow-r"><i class="fas fa-eye"></i> About<i class="fas fa-angle-down rotate-icon"></i></a>
               <div class="collapsible-body">
